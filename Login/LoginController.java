a06837e1e5d5bef4a4548280cf535b1b60805081
groundp <bloom0819@naver.com>
2022-04-01 01:09:30 +0900

    process Login Validate

--- Login/LoginController.java
+++ Login/LoginController.java
@@ -1,25 +1,58 @@
 package Login;
 
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
 import java.util.Scanner;
 
 public class LoginController {
-    public boolean processSignUp(){
+    HashMap<String, String> accountsMap = new HashMap<>();
+
+    public boolean processSignUp() {
         System.out.println("<< SIGN UP >>");
-        String id = new String();
-        String pw = new String();
-        inputAuthInfo(id, pw);
-        return true;
+        StringBuilder id2 = new StringBuilder();
+        StringBuilder pw2 = new StringBuilder();
+        if ( inputAuthInfo(id2, pw2) ) {
+            accountsMap.put(id2.toString(), pw2.toString());
+            return true;
+        }
+        else {
+            System.out.println("Failed to signUp(" + id2 +", " + pw2 +")");
+            return false;
+        }
     }
 
     public boolean processSignIn(){
-        return true;
+        System.out.println("<< SIGN IN >>");
+        StringBuilder id2 = new StringBuilder();
+        StringBuilder pw2 = new StringBuilder();
+        if ( inputAuthInfo(id2, pw2) ) {
+            if (validateLogin(id2.toString(), pw2.toString())) {
+                System.out.println("Success to signIn(" + id2 + ")");
+                return true;
+            }
+            else {
+                System.out.println("Wrong ID or PW(" + id2 + ")");
+                return false;
+            }
+        }
+        else {
+            System.out.println("Failed to signIn(" + id2 + ")");
+            return false;
+        }
     }
 
     public boolean showAccountList(){
+        int idx = 1;
+        for (Map.Entry<String, String> entry : accountsMap.entrySet() ) {
+            System.out.println(idx + ": " + entry.getKey());
+            idx++;
+        }
+
         return true;
     }
 
-    boolean inputAuthInfo(String id, String pw){
+    boolean inputAuthInfo(StringBuilder id, StringBuilder pw) {
         Scanner sc = new Scanner(System.in);
         System.out.print("ID: ");
         final String inputID = sc.next();
@@ -30,8 +63,20 @@ public class LoginController {
         if ( inputID.isEmpty() || inputPW.isEmpty() )
             return false;
 
-        id = inputID;
-        pw = inputPW;
+        id.append(inputID);
+        pw.append(inputPW);
+        return true;
+    }
+
+    boolean validateLogin(final String id, final String pw) {
+        if (!accountsMap.containsKey(id) ) {
+            return false;
+        }
+
+        if (!accountsMap.get(id).equals(pw)) {
+            return false;
+        }
+
         return true;
     }
 
--- MainController.java
+++ MainController.java
@@ -21,22 +21,23 @@ public class MainController {
     }
 
     boolean start(){
-        System.out.println("<< MAIN >>");
-        System.out.println("1. 회원가입");
-        System.out.println("2. 로그인");
-        System.out.println("3. 회원리스트 조회");
-        System.out.println("4. 종료");
+        while ( true )
+        {
+            System.out.println("<< MAIN >>");
+            System.out.println("1. 회원가입");
+            System.out.println("2. 로그인");
+            System.out.println("3. 회원리스트 조회");
+            System.out.println("4. 종료");
 
-        Scanner scanner = new Scanner(System.in);
-        final int select = scanner.nextInt();
-        switch (CATEGORY.values()[select]) {
-            case SIGNUP -> loginController.processSignUp();
-            case SIGNIN -> loginController.processSignIn();
-            case LIST -> loginController.showAccountList();
-            case EXIT -> System.exit(0);
-            default -> throw new IllegalStateException("Unexpected value: " + CATEGORY.values()[select]);
+            Scanner scanner = new Scanner(System.in);
+            final int select = scanner.nextInt();
+            switch (CATEGORY.values()[select]) {
+                case SIGNUP -> loginController.processSignUp();
+                case SIGNIN -> loginController.processSignIn();
+                case LIST -> loginController.showAccountList();
+                case EXIT -> System.exit(0);
+                default -> throw new IllegalStateException("Unexpected value: " + CATEGORY.values()[select]);
+            }
         }
-
-        return true;
     }
 }
