600bb1c0c67a85e95948b38c859e4d97e2dea944
groundp <bloom0819@naver.com>
2022-04-03 22:19:55 +0900

    process total email process

--- Email/EmailController.java
+++ Email/EmailController.java
@@ -1,9 +1,6 @@
 package Email;
 
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Scanner;
-import java.util.Set;
+import java.util.*;
 
 class Mail{
     int id;
@@ -12,14 +9,23 @@ class Mail{
     String title;
     String contents;
     boolean attached;
+
+    Mail(int id, String from, String to, String title, String contents, boolean attached) {
+        this.id = id;
+        this.from = from;
+        this.to = to;
+        this.title = title;
+        this.contents = contents;
+        this.attached = attached;
+    }
 }
 
 
 public class EmailController {
-    HashMap<String, Mail> mailList = new HashMap<>();;
+    HashMap<String, ArrayList<Mail>> mailList = new HashMap<>();
     String curAccount = "";
 
-    public boolean run(Set<String> accounts) {
+    public void run(Set<String> accounts) {
         System.out.println("<< HOME (" + curAccount + ") >>");
 
         while (true) {
@@ -33,8 +39,8 @@ public class EmailController {
 
             switch (input) {
                 case 1: showMailList(); break;
-                case 2: sendMail(); break;
-                case 3: return true;
+                case 2: sendMail(accounts); break;
+                case 3: return;
                 case 4: System.exit(0);
                 default:
             }
@@ -49,26 +55,64 @@ public class EmailController {
     void showMailList() {
         System.out.println("<< MAIL LIST >>");
 
-        if ( mailList.isEmpty() ) {
+        if ( !mailList.containsKey(curAccount) ) {
             System.err.println("There's no mail("+curAccount+")");
             return;
         }
 
-        for (Map.Entry<String, Mail> entry : mailList.entrySet() ) {
-            Mail v = entry.getValue();
+        ArrayList<Mail> v = mailList.get(curAccount);
+        System.out.println("Total mail count: "+ v.size());
+        for ( Mail m : v ) {
             System.out.println("----------------------");
-            System.out.println("id:" + v.id);
-            System.out.println("From:" + v.from);
-            System.out.println("To:" + v.to);
-            System.out.println("Title:" + v.title);
-            System.out.println("Contents:" + v.contents);
-            System.out.println("Attached:" + (v.attached ? "y" : "n"));
+            System.out.println("id:" + m.id);
+            System.out.println("From:" + m.from);
+            System.out.println("To:" + m.to);
+            System.out.println("Title:" + m.title);
+            System.out.println("Contents:" + m.contents);
+            System.out.println("Attached:" + (m.attached ? "y" : "n"));
+        }
+    }
+
+    void sendMail(Set<String> accounts) {
+        System.out.println("<< SEND MAIL >>");
+        Scanner sc = new Scanner(System.in);
+        System.out.print("To: ");
+        String To = sc.next();
+
+        if ( !validateTarget(accounts, To)) {
+            System.err.println("[ERROR] Invalid target("+To+")");
+            return;
         }
 
+        System.out.print("title: ");
+        String Title = sc.next();
+        System.out.print("contents: ");
+        String Contents = sc.next();
+        System.out.print("attached(y/n)");
+        String attached = sc.next();
+
+        if ( !mailList.containsKey(To) ) {
+            mailList.put(To, new ArrayList<>());
+        }
+
+        mailList.get(To).add(new Mail(getMaxId(), curAccount, To, Title, Contents, Objects.equals(attached, "y")));
+
     }
 
-    void sendMail() {
-
+    boolean validateTarget(Set<String> accounts, String to) {
+        return accounts.contains(to);
     }
 
+    int getMaxId() {
+        if ( mailList.isEmpty() )
+            return 1;
+
+        int maxId = 1;
+        for (Map.Entry<String, ArrayList<Mail>> entry : mailList.entrySet()) {
+            for ( final Mail m : entry.getValue() )
+                maxId = Math.max(maxId, m.id);
+        }
+
+        return maxId+1;
+    }
 }
